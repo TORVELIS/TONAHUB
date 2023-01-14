b254cf918e75961a1d94d97ecac7f70ed93a4baf
TORVELIS <89320445+TORVELIS@users.noreply.github.com>
2023-01-15 00:53:33 +0200

    Update games.lua

--- games.lua
+++ games.lua
@@ -1,1 +1,7 @@
+local PlaceID = game.PlaceId
 
+if PlaceID == 537413528 then --Build a boat for treasure
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/TORVELIS/elerium-UI-BABFT/main/OBF-MainScript.lua"))()
+elseif PlaceID == 8260276694 or PlaceID == 10201596675 then --Ability Wars
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/TORVELIS/Ability-Wars1/main/Ability%20Wars2.lua"))()
+end
